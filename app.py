a9e1d09a42f43bf8fc4d1bcb0267a7819a9d4af4
Alessandro Gallina <alessandrogallina1992@gmail.com>
2020-06-10 21:34:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -273,8 +273,8 @@ app.layout = html.Div([ #Main Container
         #dbc.Button("Relevant info", id="open-centered-left", className="btn "),
         dbc.ButtonGroup(
             [
-                dbc.Button("Home", href="https://www.learningfromthecurve.net/", external_link=True, className="py-2"),
-                dbc.Button("Dashboards", href="https://www.learningfromthecurve.net/Dashboards/", external_link=True, className="py-2"),
+                dbc.Button("Home", href="https://www.learningfromthecurve.net/", target="_top", external_link=True, className="py-2"),
+                dbc.Button("Dashboards", href="https://www.learningfromthecurve.net/Dashboards/", target="_top", external_link=True, className="py-2"),
             ],
             vertical=True,
             size="sm",
